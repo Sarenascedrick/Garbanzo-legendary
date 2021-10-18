2071e8b90afcaa1158705cc7bdb601cd00a618f6
Meagan Gabayan <201980335@psu.palawan.edu.ph>
2021-10-18 09:51:50 +0800

    Practice 5-01 done

---
+++ web2/fav.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Your Favorite</title></head>
+<body>
+
+ Thanks for your selection 
+ <?php echo $_POST ["username"];?>
+<br>
+You really enjoy <?php echo $_POST ["dish"]; ?>
+<br>
+ -especially with a nice <?php echo $_POST ["color"]; ?> wine"
+  
+</body>
+</html>
