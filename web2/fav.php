8bb0cf3cf68eef0a594cd5ebda16284463879c7c
Meagan Gabayan <201980335@psu.palawan.edu.ph>
2021-10-18 09:59:31 +0800

    prac 5-01 done

--- web2/fav.php
+++ web2/fav.php
@@ -3,9 +3,15 @@
 <body>
 
  Thanks for your selection 
- <?php echo $_POST ["username"];?>
+ <?php
+ if(isset($_POST ["username"])){
+ 	echo $_POST ["username"];
+ }
+ ?>
 <br>
-You really enjoy <?php echo $_POST ["dish"]; ?>
+You really enjoy 
+<?php 
+	echo $_POST ["dish"]; ?>
 <br>
  -especially with a nice <?php echo $_POST ["color"]; ?> wine"
   
